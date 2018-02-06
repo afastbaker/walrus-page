6cb0c2851c33235951a4e251eb8ed4d77b4eafa7
Alex Baker and Jasmine Gadie <alex@>
2018-02-05 20:35:15 -0800

    walrus_page start

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+console.log("I'm Working!");
+
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("This is a header.");
+  });
+
+  $(".clickable").click(function() {
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").toggle();
+  });
+
+  $("img").click(function() {
+    alert("This is an image.");
+  });
+});
